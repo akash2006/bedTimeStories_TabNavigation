58a3303f407a7f65b0d4f3d66305fcb93fe3fa42
Akash Maity <mightyakash124@gmail.com>
2020-10-03 15:40:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Header from 'react-native-elements'
 
 import ReadScreen from './screens/readScreen'
 import WriteScreen from './screens/writeScreen'
+import MyHeader from './components/myheader'
  
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppContainer/>
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <MyHeader/>
+        <AppContainer/>
+      </View>
+    );
+  }
 }
 
 const TabNavigator = createBottomTabNavigator({
